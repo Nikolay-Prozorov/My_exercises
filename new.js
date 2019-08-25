4017309aca6c7a55f02e5201e44eef37f32333fe
Mykola Prozorov <prozorovmykola@gmail.com>
2019-08-25 22:13:06 +0300

    First animation

--- new.js
+++ new.js
@@ -1,5 +1,20 @@
-var a = 1, b = '1, ';
-//a!==b ? document.write("true!") : document.write("false!");
-b = prompt("Do you really want to leave this page?");
-document.write(b);
+window.onload = function() {
+    var pos = 0;
 
+    var box = document.getElementById("box");
+
+    var t = setInterval(move, 10);
+
+    function move() {
+        if (pos>=150) {
+            clearInterval(t);
+            box.style.left = 0+"px";
+            box.style.top = 0+"px";
+        }
+        else {
+            pos ++;
+            box.style.left = pos+"px";
+            box.style.top = pos+"px";
+        }
+    }   
+}
